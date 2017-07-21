202f795a4f581ff52f8b01435b29d2eb625744b8
hoodielive <hoodielive@gmail.com>
2017-07-20 22:04:48 -0400

    name_value pairs and syntax_parsing.js

---
+++ Execution-Contexts-Lexical-Environments/name_value_pairs.js
@@ -0,0 +1,22 @@
+# Name/Value Pairs = Objects - names that map to a unique value (and can only have 1 value in any given context)
+
+# The value can be more than one value pair 
+# Address = '100 Main Street' name(address) 100(value) 
+# Object, a collection of name/value pairs 
+# Name = value (may have other names and values)
+	# Name = value (may have other names and values)
+		# Name = value (may have other names and values) 
+
+Address:
+	{
+		street: 'Main'
+		number: '100
+		apartment:
+			{ 
+				floor: 3, 
+				number: 301, 
+			}
+
+	}
+
+
