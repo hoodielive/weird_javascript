134179785d59da128e14e272e95c893b137066eb
hoodielive <hoodielive@gmail.com>
2017-07-27 21:11:39 -0400

    Javascript index, package, webpack configs

--- Javascript-Basics/es6/app/index.js
+++ Javascript-Basics/es6/app/index.js
@@ -1,1 +1,1 @@
-console.log('hello Aeon! (from webpack)');
+console.log('hello Aeon! (from webpack) dope - this is dope dope stuff');
--- Javascript-Basics/es6/webpack.config.js
+++ Javascript-Basics/es6/webpack.config.js
@@ -1,20 +1,21 @@
 module.exports = {
-    entry: ['./app/index.js'],
+    entry: ["./app/index.js"],
     output: {
-        path: '/home/tamauri/Documents/Programming/Javascript/git-projects/weird_javascript/Javascript-Basics/es6/build',
-        filename:'bundle.js'
+        path: "/home/tamauri/Documents/Programming/Javascript/git-projects/weird_javascript/Javascript-Basics/es6/build",
+        filename:"bundle.js"
     },
     module: {
         loaders: [
-            loader: 'babel-loader',
+	{
+            loader: "babel-loader",
             test: /\.js$/,
-            exclude: '/home/tamauri/Documents/Programming/Javascript/git-projects/weird_javascript/Javascript-Basics/es6/node_modules'
+            exclude: "/home/tamauri/Documents/Programming/Javascript/git-projects/weird_javascript/Javascript-Basics/es6/node_modules"
         }
-      ]
+      ],
   },
   devServer: {
       port: 3000,
-      contentBase: '/home/tamauri/Documents/Programming/Javascript/git-projects/weird_javascript/Javascript-Basics/es6/build',
+      contentBase: "/home/tamauri/Documents/Programming/Javascript/git-projects/weird_javascript/Javascript-Basics/es6/build",
       inline: true
   }
 }
