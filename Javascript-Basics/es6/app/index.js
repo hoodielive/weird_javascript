639cace19bd1ecde4cdfee59032fe1aa02bf5ffe
hoodielive <hoodielive@gmail.com>
2017-08-05 20:56:46 -0400

    Added placeholder for new concept

--- Javascript-Basics/es6/app/index.js
+++ Javascript-Basics/es6/app/index.js
@@ -1,1 +1,13 @@
-console.log('hello Aeon! (from webpack) dope - this is dope dope stuff');
+const emails = ['frodo@gmail.com', 'sam@gmail.com', 'mary@gmail.com']
+emails.push('pippin@gmail.com');
+console.log(emails);
+
+// block scoping
+let limit = 200;
+
+{
+    let limit = 10;
+    console.log(limit);
+}
+
+console.log(limit); 
---
+++ Javascript-Basics/es6/app/newindex.js
@@ -0,0 +1,2 @@
+// Placeholder for the new index once Ive had a chance to draft the concept 
+
